5348c83e6633b79403edcc785599082188a68fb7
Justin Dell <dell.justin@gmail.com>
2012-03-18 20:53:59 -0500

    fixing eliminated bug

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,15 +18,14 @@ class Game < ActiveRecord::Base
     puts "away_team: #{away_team.match(/teams\/(.*)\"/)[1]} away2: #{away_team.match(/\">(.*)<\/a>/)[1]}"
     self.away = Team.find_or_create_by_code(:code => away_team.match(/teams\/(.*)\"/)[1], :name => away_team.match(/\">(.*)<\/a>/)[1])
     self.home = Team.find_or_create_by_code(:code => home_team.match(/teams\/(.*)\"/)[1], :name => home_team.match(/\">(.*)<\/a>/)[1])
-    away_points = 0
-    home_points = 0
+    home_points = response.css('#ysp-reg-box-line_score .home em').inner_html.to_i
+    away_points = response.css('#ysp-reg-box-line_score .away em').inner_html.to_i
     response.css('#ysp-reg-box-game_details-game_stats').css('.bd').first.search('tbody tr').each do |p|
       player_data = p.search('a').to_html
       player = Player.find_or_create_by_yahoo_id(:yahoo_id => player_data.match(/[0-9]+/)[0],
                                                  :last_name => player_data.match(/>.* (.*)<\/a>/)[1],
                                                  :team_id => self.away.to_param)
       player.update_attributes! :points => player.points + p.search('td').last.text.to_i
-      away_points += player.points
     end
     response.css('#ysp-reg-box-game_details-game_stats').css('.bd').last.search('tbody tr').each do |p|
       player_data = p.search('a').to_html
@@ -34,7 +33,6 @@ class Game < ActiveRecord::Base
                                                  :last_name => player_data.match(/>.* (.*)<\/a>/)[1],
                                                  :team_id => self.home.to_param)
       player.update_attributes! :points => player.points + p.search('td').last.text.to_i
-      home_points += player.points
     end
     home_points > away_points ? self.away.update_attributes!(:eliminated => true) : self.home.update_attributes!(:eliminated => true)
   end
