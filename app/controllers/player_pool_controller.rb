0d05191ccc3a38869cca271106611431dc98a3d4
Justin Dell <dell.justin@gmail.com>
2012-03-18 22:28:13 -0500

    trying to fix no route matching all.css error

--- app/controllers/player_pool_controller.rb
+++ app/controllers/player_pool_controller.rb
@@ -8,7 +8,7 @@ class PlayerPoolController < ApplicationController
 
   def refresh
     raise "must specify a date (YYYY-MM-DD)" unless params[:date]
-    puts "fetching response from http://rivals.yahoo.com/ncaa/basketball/scoreboard?d=#{params[:date]}"
+    Rails.logger.info "fetching response from http://rivals.yahoo.com/ncaa/basketball/scoreboard?d=#{params[:date]}"
     http_response = Net::HTTP.get_response(URI.parse("http://rivals.yahoo.com/ncaa/basketball/scoreboard?d=#{params[:date]}"))
     raise "ERROR: HTTP RESPONSE STATUS NOT 200: #{http_response.body}" if http_response.code != '200'
     doc = Nokogiri::HTML(http_response.body)
