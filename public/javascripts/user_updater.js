cb108c050e8a2692e00a5fb4f190162486623a2c
Justin Dell <dell.justin@gmail.com>
2012-03-22 12:44:57 -0500

    more highlighting

--- public/javascripts/user_updater.js
+++ public/javascripts/user_updater.js
@@ -1,10 +1,10 @@
-function refresher(refresh_date) {
+function refresher(refresh_date, user_id) {
   setInterval(function(){
     current_points = [];
     for(i=0;i<$('table tbody tr').length; i++) {
       current_points[i] = parseInt($('table tbody tr')[i].children[2].innerHTML);
     }
-    $('#standings').load("/refresh?date=" + refresh_date);
+    $('#players').load("/refresh?date=" + refresh_date + "&user_id=" + user_id);
     for(i=0;i<$('table tbody tr').length; i++) {
       new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
       if(current_points[i] < new_points) {
