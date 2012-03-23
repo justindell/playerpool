efa75cc7febb30e069f213a71a7ef342dad20104
Justin Dell <dell.justin@gmail.com>
2012-03-22 19:14:13 -0500

    trying to fix flash

--- public/javascripts/player_updater.js
+++ public/javascripts/player_updater.js
@@ -4,13 +4,14 @@ function refresher(refresh_date) {
     for(i=0;i<$('table tbody tr').length; i++) {
       current_points[i] = parseInt($('table tbody tr')[i].children[2].innerHTML);
     }
-    $('#standings').load("/refresh?date=" + refresh_date);
-    for(i=0;i<$('table tbody tr').length; i++) {
-      new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
-      if(current_points[i] < new_points) {
-        $($('table tbody tr')[i].children[2]).effect('highlight', {color: "#66FF66"}, 1000);
-        current_points[i] = new_points;
+    $('#standings').load("/refresh?date=" + refresh_date, function(){
+      for(i=0;i<$('table tbody tr').length; i++) {
+        new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
+        if(current_points[i] < new_points) {
+          $($('table tbody tr')[i].children[2]).effect('highlight', {color: "#66FF66"}, 1000);
+          current_points[i] = new_points;
+        }
       }
-    }
+    });
   }, 30000);
 }
