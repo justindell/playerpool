8f29c89fa16942f38f0605d4b2cec603cc98fd68
Justin Dell <dell.justin@gmail.com>
2013-02-07 16:45:27 -0600

    update to rails 3.2.11

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.10'
+gem 'rails', '3.2.11'
 
 gem 'sqlite3'
 gem 'nokogiri'
@@ -8,9 +8,9 @@ gem 'haml'
 gem 'jquery-rails'
 
 group :assets do
-  gem 'sass-rails', '~> 3.1.0'
-  gem 'coffee-rails', '~> 3.1.0'
-  gem 'uglifier'
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
 end
 
 group :development, :test do
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Playerpool::Application.config.secret_token = '9c9e461dca3e93a6a28a972cc54cfc5caa11fe266f9de0b6666d4ee6a83384881f3ccdfa68dd62a07fa2ffc7f26dc7b92c688a16bf30bf5f2a32d396bb60b97f'
+Playerpool::Application.config.secret_token = '9d676246c046093f1f40339de541f84604b6a35aeea4d1c0669d67980d9034636bfbf2d759878cf4d58d0aa5420dfa3a5702764fd6018ad88d91c2dab3ad263c'
