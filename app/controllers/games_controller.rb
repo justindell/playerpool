547bb393bb02939ec14c26d06f05b8bc87561da4
Justin Dell <dell.justin@gmail.com>
2013-02-09 08:19:25 -0600

    xml->json

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,34 +1,34 @@
 class GamesController < ApplicationController
   # GET /games
-  # GET /games.xml
+  # GET /games.json
   def index
     @games = Game.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @games }
+      format.json  { render :json => @games }
     end
   end
 
   # GET /games/1
-  # GET /games/1.xml
+  # GET /games/1.json
   def show
     @game = Game.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @game }
+      format.json  { render :json => @game }
     end
   end
 
   # GET /games/new
-  # GET /games/new.xml
+  # GET /games/new.json
   def new
     @game = Game.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @game }
+      format.json  { render :json => @game }
     end
   end
 
@@ -38,33 +38,33 @@ class GamesController < ApplicationController
   end
 
   # POST /games
-  # POST /games.xml
+  # POST /games.json
   def create
     @game = Game.new(params[:game])
 
     respond_to do |format|
       if @game.save
         format.html { redirect_to(@game, :notice => 'Game was successfully created.') }
-        format.xml  { render :xml => @game, :status => :created, :location => @game }
+        format.json  { render :json => @game, :status => :created, :location => @game }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @game.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @game.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /games/1
-  # PUT /games/1.xml
+  # PUT /games/1.json
   def update
     @game = Game.find(params[:id])
 
     respond_to do |format|
       if @game.update_attributes(params[:game])
         format.html { redirect_to(@game, :notice => 'Game was successfully updated.') }
-        format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @game.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @game.errors, :status => :unprocessable_entity }
       end
     end
   end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,34 +1,34 @@
 class PlayersController < ApplicationController
   # GET /players
-  # GET /players.xml
+  # GET /players.json
   def index
     @players = Player.includes(:team).all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @players.to_xml(:methods => [:full_name, :team_name], :only => [:id]) }
+      format.json  { render :json => @players.to_json(:methods => [:full_name, :team_name], :only => [:id]) }
     end
   end
 
   # GET /players/1
-  # GET /players/1.xml
+  # GET /players/1.json
   def show
     @player = Player.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @player }
+      format.json  { render :json => @player }
     end
   end
 
   # GET /players/new
-  # GET /players/new.xml
+  # GET /players/new.json
   def new
     @player = Player.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @player }
+      format.json  { render :json => @player }
     end
   end
 
@@ -38,46 +38,46 @@ class PlayersController < ApplicationController
   end
 
   # POST /players
-  # POST /players.xml
+  # POST /players.json
   def create
     @player = Player.new(params[:player])
 
     respond_to do |format|
       if @player.save
         format.html { redirect_to(@player, :notice => 'Player was successfully created.') }
-        format.xml  { render :xml => @player, :status => :created, :location => @player }
+        format.json  { render :json => @player, :status => :created, :location => @player }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @player.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @player.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /players/1
-  # PUT /players/1.xml
+  # PUT /players/1.json
   def update
     @player = Player.find(params[:id])
 
     respond_to do |format|
       if @player.update_attributes(params[:player])
         format.html { redirect_to(@player, :notice => 'Player was successfully updated.') }
-        format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @player.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @player.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /players/1
-  # DELETE /players/1.xml
+  # DELETE /players/1.json
   def destroy
     @player = Player.find(params[:id])
     @player.destroy
 
     respond_to do |format|
       format.html { redirect_to(players_url) }
-      format.xml  { head :ok }
+      format.json  { head :ok }
     end
   end
 end
