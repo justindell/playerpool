11d83edf3fa929ff8082e9587ad58adee6c76d34
Justin Dell <dell.justin@gmail.com>
2014-03-19 21:36:17 -0500

    remove updater for now

--- app/assets/javascripts/user_updater.js
+++ app/assets/javascripts/user_updater.js
@@ -1,17 +1,17 @@
 function user_refresher(refresh_date, user_id) {
-  setInterval(function(){
-    current_points = [];
-    for(i=0;i<$('table tbody tr').length; i++) {
-      current_points[i] = parseInt($('table tbody tr')[i].children[2].innerHTML);
-    }
-    $('#players').load("/refresh?date=" + refresh_date + "&user_id=" + user_id, function() {
-      for(i=0;i<$('table tbody tr').length; i++) {
-        new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
-        if(current_points[i] < new_points) {
-          $($('table tbody tr')[i].children[2]).effect('highlight', {color: "#66FF66"}, 1000);
-          current_points[i] = new_points;
-        }
-      }
-    });
-  }, 60000);
+  //setInterval(function(){
+    //current_points = [];
+    //for(i=0;i<$('table tbody tr').length; i++) {
+      //current_points[i] = parseInt($('table tbody tr')[i].children[2].innerHTML);
+    //}
+    //$('#players').load("/refresh?date=" + refresh_date + "&user_id=" + user_id, function() {
+      //for(i=0;i<$('table tbody tr').length; i++) {
+        //new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
+        //if(current_points[i] < new_points) {
+          //$($('table tbody tr')[i].children[2]).effect('highlight', {color: "#66FF66"}, 1000);
+          //current_points[i] = new_points;
+        //}
+      //}
+    //});
+  //}, 60000);
 }
