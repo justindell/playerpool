b6cfa751f44edef5451375b9f385297c70dae593
Justin Dell <dell.justin@gmail.com>
2015-03-18 17:17:16 -0500

    Revert "disable updating"
    This reverts commit b2b98558ae354fd4f680ac7ead1f9d0b287134f3.

--- app/assets/javascripts/player_updater.js
+++ app/assets/javascripts/player_updater.js
@@ -1,19 +1,19 @@
 // TODO lots of duplication with user updater
 
-//function player_refresher(refresh_date) {
-  //setInterval(function(){
-    //current_points = [];
-    //for(i=0;i<$('table tbody tr').length; i++) {
-      //current_points[i] = parseInt($('table tbody tr')[i].children[2].innerHTML);
-    //}
-    //$('#standings').load("/refresh?date=" + refresh_date, function(){
-      //for(i=0;i<$('table tbody tr').length; i++) {
-        //new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
-        //if(current_points[i] < new_points) {
-          //$($('table tbody tr')[i].children[2]).effect('highlight', {color: "#66FF66"}, 1000);
-          //current_points[i] = new_points;
-        //}
-      //}
-    //});
-  //}, 60000);
-//}
+function player_refresher(refresh_date) {
+  setInterval(function(){
+    current_points = [];
+    for(i=0;i<$('table tbody tr').length; i++) {
+      current_points[i] = parseInt($('table tbody tr')[i].children[2].innerHTML);
+    }
+    $('#standings').load("/refresh?date=" + refresh_date, function(){
+      for(i=0;i<$('table tbody tr').length; i++) {
+        new_points = parseInt($('table tbody tr')[i].children[2].innerHTML)
+        if(current_points[i] < new_points) {
+          $($('table tbody tr')[i].children[2]).effect('highlight', {color: "#66FF66"}, 1000);
+          current_points[i] = new_points;
+        }
+      }
+    });
+  }, 60000);
+}
