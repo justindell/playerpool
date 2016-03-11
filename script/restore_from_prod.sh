f440d08b31f2ab793b42c3b148ffa43b58d6ef09
Justin Dell <dell.justin@gmail.com>
2016-03-11 12:00:10 -0600

    latest updates

--- script/restore_from_prod.sh
+++ script/restore_from_prod.sh
@@ -2,7 +2,7 @@
 
 set -v
 
-heroku pgbackups:capture
-curl -o latest.dump `heroku pgbackups:url`
+heroku pg:backups capture
+curl -o latest.dump `heroku pg:backups public-url`
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U myuser -d mydb COLOR_54d4041969702d0295380301.dump
 rm latest.dump
