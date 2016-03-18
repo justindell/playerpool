3d1745f1664efaea571feef71c1a0730c6e40f99
Justin Dell <dell.justin@gmail.com>
2016-03-18 09:19:43 -0500

    new yahoo site

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,7 +11,7 @@ class Game < ActiveRecord::Base
   def fetch_game_attributes
     Rails.logger.info "fetching game attributes for #{self.url}"
     http_response = Net::HTTP.get_response(URI.parse(self.url))
-    http_response = Net::HTTP.get_response(URI.parse(http_response['location'] + '/')) if http_response.is_a? Net::HTTPRedirection 
+    http_response = Net::HTTP.get_response(URI.parse(http_response['location'] + '/')) if http_response.is_a? Net::HTTPRedirection
     raise "ERROR: HTTP RESPONSE STATUS NOT 200: #{http_response.code} - #{http_response.body}" if http_response.code != '200'
     response = Nokogiri::HTML(http_response.body)
     away_team = response.search("#mediamodulematchheadergrandslam .teams-container .team.away a").last.to_html
@@ -40,8 +40,8 @@ class Game < ActiveRecord::Base
   end
 
   def find_or_create_player player_data, team
-    Player.create_with(:first_name => player_data.match(/>(.*) (.*)<\/a>/)[1],
-                       :last_name => player_data.match(/>(.*) (.*)<\/a>/)[2],
+    Player.create_with(:first_name => player_data.match(/data-entity-display-name="(\S*) (.*)" data-entity-league/)[1],
+                       :last_name => player_data.match(/data-entity-display-name="(\S*) (.*)" data-entity-league/)[2],
                        :team_id => self.send(team).to_param).
            find_or_create_by(:yahoo_id => player_data.match(/[0-9]+/)[0])
   end
